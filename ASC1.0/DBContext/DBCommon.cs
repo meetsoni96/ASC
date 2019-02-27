2a23036300fe02a11c24d1c5f57fb76ba1c2234a
meetsoni96 <meetsoni01041996@gmail.com>
2019-02-27 20:25:37 +0530

    Added DB commn class for Database operations

---
+++ ASC1.0/DBContext/DBCommon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASC1._0.DBContext
+{
+    /// <summary>
+    /// Handling DB related operations
+    /// </summary>
+    public class DBCommon
+    {
+    }
+}
