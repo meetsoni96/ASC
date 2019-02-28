14a0d6ba1ed4e1dbdfe8025252f57f829f5d5744
Santosh Singh <santoshs@markettrack.com>
2019-02-28 17:00:17 +0530

    Added database changes for Products and Domian

---
+++ ASC1.0/DBContext/DomainDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASC1._0.DBContext
+{
+    public class DomainDataAccess
+    {
+        DBManager objDBManager = new DBManager();
+        DataTable dt = new DataTable();
+        string query = string.Empty;
+
+        public DataTable getALLDomains()
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = "dbo.GetDomains";
+            return objDBManager.ExecuteDataTable(sqlCommand);
+        }
+    }
+}
---
+++ ASC1.0/DBContext/ProductsDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASC1._0.DBContext
+{
+    public class ProductsDataAccess
+    {
+        DBManager objDBManager = new DBManager();
+        DataTable dt = new DataTable();
+        string query = string.Empty;
+        public void SaveProductsInDomain(Products product)
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = "dbo.AddProducts";
+            sqlCommand.Parameters.AddWithValue("@Category_ID", product.CategoryID);
+            sqlCommand.Parameters.AddWithValue("@Domain_ID", product.DomainID);
+            sqlCommand.Parameters.AddWithValue("@Title", product.Title);
+            sqlCommand.Parameters.AddWithValue("@Price", product.Price);
+            sqlCommand.Parameters.AddWithValue("@Strike_Through_Price", product.StrikeThroughPrice);
+            sqlCommand.Parameters.AddWithValue("@SKU", product.SKU);
+            sqlCommand.Parameters.AddWithValue("@MPN", product.MPN);
+            sqlCommand.Parameters.AddWithValue("@Product_Url", product.ProductUrl);
+            sqlCommand.Parameters.AddWithValue("@Availability", product.Availability);
+            objDBManager.ExecuteNonQuery(sqlCommand);
+            sqlCommand.Dispose();
+
+        }
+
+        public DataTable GetProductsByMPNOrTitle(string id, string txtSearch)
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            if (id == "1")
+            {
+                sqlCommand.CommandText = "dbo.GetProductsByMPNOrTitle";
+                sqlCommand.Parameters.AddWithValue("@MPN", txtSearch);
+            }
+            else if (id == "2")
+            {
+                sqlCommand.CommandText = "dbo.GetProductsByMPNOrTitle";
+                sqlCommand.Parameters.AddWithValue("@Title", txtSearch);
+
+            }
+
+            return objDBManager.ExecuteDataTable(sqlCommand); ;
+        }
+    }
+}
